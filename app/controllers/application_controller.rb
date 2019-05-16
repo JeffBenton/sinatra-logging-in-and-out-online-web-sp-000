5277a60bd25a4d065c51da1419b8f62f3b8f2b6b
Jeff Benton <sikletz@gmail.com>
2019-05-16 19:15:10 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,17 +21,24 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
+<<<<<<< HEAD
     if session[:user_id]
       @session = session
       erb :account
     else
       erb :error
     end
+=======
+    erb :account
+>>>>>>> 5fa239f21c60bcf4cc5a9817821c314ed9e90124
   end
 
   get '/logout' do
     session.clear
+<<<<<<< HEAD
     redirect "/"
+=======
+>>>>>>> 5fa239f21c60bcf4cc5a9817821c314ed9e90124
   end
 
 
